5bc7c6d1f1326dc76e9e298250cf849ee2fd0b19
freq-mod <32672779+freq-mod@users.noreply.github.com>
2023-06-04 21:49:43 +0200

    Add files via upload

--- homepage/contact.php
+++ homepage/contact.php
@@ -8,11 +8,35 @@
 <body>
   <div id="Glowny">
   <?php include 'header.php';?>
-    <h1> Contact </h1>
+  <div id ="info">
+  <center>
+    <h2> Contact </h2>
+</center>
     <p> If you must...</p>
     <a href="https://www.youtube.com/channel/UCf6hxjn9Dpwhi3BdtQXvQTw">
   <img src="YouTube-Icon-Full-Color-Logo.wine.svg" alt="" style="width:128px;height:128px;">
 </a>
+<a href="https://github.com/freq-mod">
+  <img src="25231.png" alt="" style="width:128px;height:128px;">
+</a>
+<br>
+<center>
+    <h3> E-mail </h3>
+</center>
+<p> Solve the following:</p> <br>
+<p> Ball and paddle cost 1.10 USD altogether. The paddle is 1 USD more expensive than the ball. How much does the ball cost? <small> note for retards: 1 usd = 100 cents </small><p>
+<form action="contact.php" method="post">
+<input type="text" name="inputbox" value="0" />
+<input type="submit">
+</form>
+<?php
+$inputval = @$_POST["inputbox"];
+if ($inputval==5 or $inputval==0.05)
+echo "nwd937172@gmail.com";
+elseif ($inputval==0) echo "";
+else  echo "jubao@people.cn";
+?>
+</div>
 </div>
 </body>
 </html>
--- homepage/works.php
+++ homepage/works.php
@@ -9,15 +9,17 @@
 <style type="text/css">
 .tg  {border-collapse:collapse;border-spacing:0;}
 .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  overflow:hidden;padding:10px 5px;word-break:normal;}
+  overflow:hidden;padding:10px 15px;word-break:normal;}
 .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+  font-weight:normal;overflow:hidden;padding:10px 15px;word-break:normal;}
 .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
 </style>
 <body>
   <div id="Glowny">
   <?php include 'header.php';?>
+  <center>
     <h2> My works </h2>
+</center>
     <p> Here is the archive of my past and previous arranges.
     <table class="tg">
 <thead>
