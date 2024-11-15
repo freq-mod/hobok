b532085211b3708446cf7d5c0ad32d9f7ad197fa
freq-mod <32672779+freq-mod@users.noreply.github.com>
2024-11-15 19:42:34 +0100

    Update about.php

--- homepage/about.php
+++ homepage/about.php
@@ -14,7 +14,7 @@
         <h2>About</h2> <font color="#c3bdff">
         </center>
         <!--<img src=".jpg"> -->
-        <p>I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces.</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         
         </div>
 </body>
